c51f49f9fbfd478a673bf5f8f80e530ea245f6e9
Rafa Villar <villarrafael@gmail.com>
2021-03-05 22:16:50 -0300

    config allowed hosts

--- djproj/settings/dev.py
+++ djproj/settings/dev.py
@@ -1,6 +1,6 @@
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost', 'ravillar.pythonanywhere.com']
 
 print('Settings para entorno de desarrollo!! ')
